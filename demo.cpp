30eb1a2adecb2f33db4d0fce90963f8a2bf12467
ManthanMarathe223 <manthanmarathe223@gmail.com>
2025-09-24 21:20:41 +0530

    Implement merge sort and quick sort algorithms; update main function to demonstrate sorting

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+// Insertion Sort
 void insertionSort(int arr[], int n){
 
     for (int i=1; i<n; i++){
@@ -19,10 +21,60 @@ void insertionSort(int arr[], int n){
     }
 }
 
+// Merge Sort
+void merge(int arr[], int low, int mid, int high){
+
+    int left = low;
+    int right = mid+1;
+
+    vector<int> temp;
+
+    while(left<=mid && right<=high){
+
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while(right<=high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i=low; i<=high; i++){
+        arr[i] = temp[i-low];
+    }
+}
+
+void mergeSort(int arr[], int low, int high){
+
+    if(low == high) return;
+
+    int mid = (low+high) / 2;
+
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+
 int main(){
     int arr[] = {2,4,3,1,5,6};
 
-    insertionSort(arr,6);
+    mergeSort(arr, 0, 5);
+
+    for(int val: arr){
+        cout << val << " ";
+    }
 
     return 0;
 }
--- demo1.cpp
+++ demo1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+// Shell Sort
 void shellSort(int arr[], int n){
 
     for(int gap=n/2; gap>=1; gap=gap/2){
@@ -21,10 +22,43 @@ void shellSort(int arr[], int n){
     }
 }
 
+// Quick Sort
+
+int findPivot(int arr[], int st, int end){
+
+    int idx = st-1;
+    int pivot = arr[end];
+
+    for(int j=st; j<end; j++){
+
+        if(arr[j]<=pivot){
+            idx++;
+            swap(arr[j], arr[idx]);
+        }
+    }
+
+    idx++;
+    swap(arr[end], arr[idx]);
+
+    return idx;
+}
+
+void quickSort(int arr[], int st, int end){
+
+    if(st<end){
+        int pivot = findPivot(arr,st,end);
+        quickSort(arr, st, pivot-1);
+        quickSort(arr, pivot+1, end);
+    }
+}
+
 int main(){
     int arr[] = {2,4,3,1,5,6};
 
-    shellSort(arr,6);
+    quickSort(arr, 0, 5);
+    for(int i=0; i<=5; i++){
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
